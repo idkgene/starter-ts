29df5fc32bed90bb63413a2b3a77694c648478a9
Eugene <webeugene@proton.me>
2024-11-10 23:17:00 +0300

    chore: more scripts, more deps, some envs for different environments

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export const one = 1
-export const two = 2
+import dotenv from 'dotenv'
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+
+export const getApiUrl = () => process.env.API_URL
+export const isDebug = () => process.env.DEBUG === 'true'
+
+console.log(`Current environment: ${process.env.NODE_ENV}`)
+console.log(`API URL: ${getApiUrl()}`)
+console.log(`Debug mode: ${isDebug()}`)
