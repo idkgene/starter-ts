5649e6f72a484e9d4974ae84c5b70c5e92127dcb
idkgene <webeugene@proton.me>
2024-12-10 02:22:02 +0300

    chore(settings): added scripts, adapted template for Effect
    - Initialized empty husky
    - Added scripts for pre-commit (`pnpm pre-commit` and husky initialization (`pnpm prepare`)
    - Specialized the repository type and url in `package.json`
    - Set "engines" to a specific version of Node.js
    - Changed `index.ts` to display simple Hello World using Effect.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-import dotenv from 'dotenv'
-import path from 'path'
+import { Effect, Console } from 'effect';
 
-const envPath = process.env.ENV_PATH || path.resolve(__dirname, `../config/.env.${process.env.NODE_ENV}`)
+const program = Console.log('Hello World!')
 
-dotenv.config({ path: envPath })
-
-export const getApiUrl = () => process.env.API_URL
-export const isDebug = () => process.env.DEBUG === 'true'
-
-console.log(`Current environment: ${process.env.NODE_ENV}`)
-console.log(`API URL: ${getApiUrl()}`)
-console.log(`Debug mode: ${isDebug()}`)
+Effect.runSync(program)
